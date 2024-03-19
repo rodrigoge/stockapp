82de4e178fb20bb978959f320147483df5e0de63
Rodrigo Gouveia <rgestevao@gmail.com>
2024-03-19 18:36:32 -0300

    feat: starting the user service project

---
+++ api/userservice/src/test/java/com/stockapp/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.stockapp.userservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
